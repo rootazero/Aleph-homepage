2d92efc2bff242b67c191718ceb23cf3ba4f2d3e
rootazero <zouguojunx@gmail.com>
2026-06-07 10:48:44 +0800

    home: drop forced dark wrapper and unused imports in locale layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,10 +1,7 @@
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
-import { ThemeProvider } from "next-themes";
 import { routing } from "@/i18n/routing";
-import { Navbar } from "@/components/layout/Navbar";
-import { Footer } from "@/components/layout/Footer";
 
 type Props = {
   children: React.ReactNode;
@@ -19,9 +16,7 @@ export default async function LocaleLayout({ children, params }: Props) {
   const messages = await getMessages();
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
-      <div className="dark contents">
-        {children}
-      </div>
+      {children}
     </NextIntlClientProvider>
   );
 }
